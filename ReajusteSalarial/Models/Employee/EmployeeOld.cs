22e14cb9c1cedc899363c5b734a0f92933cc14a7
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-11-25 13:09:16 -0300

    Adding files to github

---
+++ ReajusteSalarial/Models/Employee/EmployeeOld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReajusteSalarial.Models.Employee
+{
+    internal class EmployeeOld : Employee
+    {
+        public EmployeeOld()
+        {
+            this.Roll = "Old";
+            
+            
+        }
+
+    }
+}
---
+++ ReajusteSalarial/Program.cs
@@ -0,0 +1,23 @@
+﻿using ReajusteSalarial.Models.Employee;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Employee func1 = new Employee();
+
+       
+        Console.WriteLine("Digite Seu nome: ");
+        func1.Name = Console.ReadLine();
+        Console.WriteLine("Seu Nome: " + func1.Name);
+
+        Console.WriteLine("Digite Seu Salario: ");
+        func1.Salary = double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite Seu Salario: " + func1.Salary);
+
+
+        Console.Read();
+
+
+    }
+}
